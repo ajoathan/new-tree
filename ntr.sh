a32321816c3c5915a22a622251454f5a5565d334
Alvaro Gomes <ajoathan@gmail.com>
2018-04-16 01:21:34 +0000

    Criado script principal: ntr.sh.
    Esse script cria as root's em "$HOME/.ntr" e cria um bind da pasta do seu
    projeto para a home da jail.

---
+++ ntr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+HME="/root"
+BINDS=""
+PTH=".:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+
+SUITE="stretch"
+ROOT="$SUITE"
+MIRROR="http://localhost:3142/ftp.br.debian.org/debian/"
+
+while [ "$1" != "" ]; do
+	if [[ "$1" == "--mirror" || "$1" == "-m" ]]; then
+		MIRROR="$2"
+	elif [[ "$1" == "--suite" || "$1" == "-s" ]]; then
+		SUITE="$2"
+	elif [[ "$1" == "--home" || "$1" == "-h" ]]; then
+		HME="$2"
+	elif [[ "$1" == "--bind" || "$1" == "-b" ]]; then
+		BINDS="$BINDS $2"
+	else
+		ROOT="$1"
+		break
+	fi
+	shift 2
+done
+
+BINDS="$BINDS $ROOT"
+for bind in $BINDS; do
+	BIND="$BIND -b $bind:$HME/`basename $bind`"
+done
+
+ROOT="$HOME/.ntr/$ROOT"
+if [[ ! -d "$ROOT" ]]; then
+	fakechroot fakeroot debootstrap "$SUITE" "$ROOT" "$MIRROR" &&
+	rm -rf "$ROOT/proc" "$ROOT/sys" "$ROOT/dev"
+fi &&
+
+env -i DISPLAY=$DISPLAY HOME="$HME" PATH="$PTH" TERM=$TERM \
+	proot -0 -w "$HME" -r "$ROOT" -b /proc -b /sys -b /dev \
+	$BIND /bin/bash
